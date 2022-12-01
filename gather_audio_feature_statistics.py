1994514f2caf7fdec3f5aa6773b997e11a426d0c
Maaike Visser <maaike@variedata.nl>
2022-12-01 22:30:46 +0100

    Get audio features for all saved songs

--- gather_audio_feature_statistics.py
+++ gather_audio_feature_statistics.py
@@ -21,16 +21,33 @@ res_limit = 50
 saved = spotify.saved_tracks(limit=res_limit)
 # print(saved.items)
 # total_tracks = saved.total
-upper_limit = roundup(saved.total, res_limit)
+upper_limit = roundup(saved.total+res_limit, res_limit)
 
-for offset in range(res_limit, upper_limit + res_limit, res_limit):
+for offset in range(res_limit, upper_limit, res_limit):
     # print(offset)
     all_tracks += saved.items
     saved = spotify.saved_tracks(offset=offset, limit=res_limit)
 
 assert len(all_tracks) == saved.total
 
-all_tracks = sorted(all_tracks, key=lambda track: track.track.popularity)
 
-for track in all_tracks:
-    print(track.track)
+# all_tracks = sorted(all_tracks, key=lambda track: track.track.popularity)
+
+with open('relevant_features.txt','r') as fp:
+    audio_features = {k.strip():[] for k in fp}
+
+
+
+afs = []
+
+rate_limit = 100
+track_ids = [track.track.id for track in all_tracks]
+for chunk in range(rate_limit, upper_limit, rate_limit):
+    afs += spotify.tracks_audio_features(track_ids[chunk - rate_limit:chunk])
+#
+for af in afs:
+    for f in audio_features:
+        audio_features[f].append(getattr(af, f))
+#
+# print(audio_features)
+print(audio_features)
